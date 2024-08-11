b867d8c405ac955bc24f52708d8fa76d6017b3f3
Bhagirath Patel <112346654+bhagirath-bhp@users.noreply.github.com>
2024-08-11 17:18:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MapComponent from "./components/MapComponent";
+
+function App() {
+  return (
+    <>
+      <MapComponent />
+      <p>hii</p>
+    </>
+  );
+}
+
+export default App;
